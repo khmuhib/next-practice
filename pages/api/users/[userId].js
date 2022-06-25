5d241086b7e19bd8bdaa62c2d32a85749bc82df9
khmuhib <khmuhib2013@gmail.com>
2022-06-25 11:55:19 +0600

    api complete

---
+++ pages/api/users/[userId].js
@@ -0,0 +1,17 @@
+import {users} from "./../../../users"
+
+export default function handler (req, res) {
+    const {userId} = req.query
+
+    if (req.method === "GET") {
+        const user = users.find((user)=>user.id == parseInt(userId));
+        res.status(200).json(user)
+    } else if (req.method === "DELETE") {
+        const deleteUser = users.find((user)=>user.id == parseInt(userId));
+        const index = users.findIndex((user)=>user.id == parseInt(userId));
+        users.splice(index, 1)
+        res.status(200).json(deleteUser)
+
+
+    }
+}
--- pages/users/index.js
+++ pages/users/index.js
@@ -5,30 +5,48 @@ import { Nav } from "../../components/Nav";
 export default function Users() {
   const [users, setusers] = useState([]);
   const [user, setuser] = useState("");
+  
   const loadAllData = () => {
     fetch("api/users")
       .then((res) => res.json())
       .then((data) => setusers(data));
   };
-  const handAddUser = () => {
-    fetch("api/users", {});
+
+  const handleAddUser = () => {
+    fetch("api/users", {
+      method: "POST",
+      body: JSON.stringify({user}),
+      headers: {"Content-Type": "application/json"},
+    });
+    loadAllData();
   };
+
+  const deleteUser = (id) => {
+    fetch(`/api/users/${id}`, {
+      method: "DELETE",
+    })
+    loadAllData();
+
+  }
+
   return (
     <div>
       <Nav></Nav>
       <h1>All Users</h1>
       <div>
-        <input type="text" onChange={(e) => setUser(e.target.value)} />
-        <button onClick={handAddUser}>Add User</button>
+        <input type="text" onChange={(e) => setuser(e.target.value)} />
+        <button onClick={handleAddUser}>Add User</button>
       </div>
       <button onClick={loadAllData}>Load Users</button>
       {users.map((user) => {
         return (
-          <>
+          <div key={user.id}>
             <h2>
               {user.id}: {user.name}
             </h2>
-          </>
+            
+            <button onClick={()=>deleteUser(user.id)}>Delete</button>
+          </div>
         );
       })}
     </div>
